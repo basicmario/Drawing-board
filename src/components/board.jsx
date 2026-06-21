dfca2a366a568671955f49e381836d7b4f6333cf
unknown <romariot2004@gmail.com>
2026-06-21 17:39:19 -0500

    fixed the drawing brush, it works smoothly now

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -5,20 +5,29 @@ import './board.css'
 
 function Board({width, height, theref, context, brushcolor, lineWidth}) {
 
-    
+
+    //storing variables
+    const pointsHolder = []
+    const draw = useRef(false)
     console.log(` the width: ${width}, ${height}, and the ref`)
     console.log(` getting stuff ${context}`)
+    const startingPosition = useRef({startingX : 0, startingY : 0})
 
-    const draw = useRef(false)
+
 
     //functions
-
-    const MouseDown = () => {
+    const MouseDown = (event) => {
         draw.current = true
+        context.current.beginPath()
+        startingPosition.current = {startingX : event.clientX, startingY : event.clientY}
     }
 
     const MouseUp = () => {
         draw.current = false
+
+        for (let x = 0; x < pointsHolder.length; x++){
+            console.log(`the points are: ${pointsHolder[x].x}, ${pointsHolder[x].y}`)
+        }
     }
 
 
@@ -31,11 +40,22 @@ function Board({width, height, theref, context, brushcolor, lineWidth}) {
             if (!context.current) return
 
             if (draw.current == false) return
-            context.current.beginPath()
-            context.current.arc(event.clientX, event.clientY, lineWidth, 0, 2 * Math.PI)
-            context.current.fillStyle = brushcolor
-            context.current.fill()
+            context.current.lineCap = "round"
+            context.current.moveTo(startingPosition.current.startingX, startingPosition.current.startingY)
+
+
+            const prev = {x: startingPosition.current.startingX, y: startingPosition.current.startingY}
+            const midX = (prev.x + event.clientX) / 2
+            const midY = (prev.y + event.clientY) / 2
+
+            context.current.quadraticCurveTo(prev.x, prev.y, midX, midY)
+            context.current.strokeStyle = brushcolor
             context.current.lineWidth = lineWidth
+            context.current.stroke()
+            startingPosition.current = {startingX : midX, startingY : midY}
+
+
+            pointsHolder.push({x : event.clientX, y: event.clientY})
             
         }
 
